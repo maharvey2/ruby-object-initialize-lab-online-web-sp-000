555112b6947c75dc44089b1133de587c5ddfe4ea
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-19 20:11:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,5 @@
 class Dog
   def initialize(breed)
     @breed = "Mutt"
-    @name
-  end
-    def breed=(breed)
-      @breed = breed
-    end
-      def breed
-        @breed
-      end
+    @name = name
 end
